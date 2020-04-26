71bf97017588e299216d8b81ed8b36e3c8444c2a
Tony Lagnada <tony@lagnada.com>
2020-04-26 09:51:30 -0700

    placeholder class for example package

---
+++ assertj-modules-example/src/main/java/com/lagnada/assertion/modules/assertj/example/Example.java
@@ -0,0 +1,9 @@
+package com.lagnada.assertion.modules.assertj.example;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("This is a placeholder class.");
+    }
+
+}
